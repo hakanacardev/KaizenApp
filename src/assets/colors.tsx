58b83798a9f6d707bf203a080b5b94db1c21c5ad
hakanacardev <hakanacar200@gmail.com>
2024-05-21 01:05:25 +0300

    install packages and images
    Installing the packages to be used and images

---
+++ src/assets/colors.tsx
@@ -0,0 +1,17 @@
+
+interface Colors {
+    red: string;
+    black: string;
+    border: string;
+    white: string;
+  }
+  
+  const colors: Colors = {
+    red: '#F40000',
+    black: '#1D1E1C',
+    border: '#ECEEEF',
+    white: '#fff'
+  }
+  
+  export default colors;
+  
